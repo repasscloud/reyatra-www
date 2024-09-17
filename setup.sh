35743b4575ff5cdfb454c8cdc32f273e41519608
Danijel-James Wynyard <danijel@repasscloud.com>
2024-09-17 12:19:21 +1000

    update for dev01 deployment

--- setup.sh
+++ setup.sh
@@ -2,14 +2,11 @@
 
 docker compose down
 
-echo -e "TeddyBear101" | sudo -S sudo rm -rf ./data/mariadb
+sudo rm -rf ./data/mariadb
 mkdir -p ./data/mariadb
+sudo touch ./conf/mariadb/.gitkeep
 
-echo -e "TeddyBear101" | sudo -S sudo chown -R 999:999 ./data/mariadb
-echo -e "TeddyBear101" | sudo -S sudo chmod -R 755 ./data/mariadb
-
-echo -e "TeddyBear101" | sudo -S sudo chown -R 999:999 ./conf/mariadb
-echo -e "TeddyBear101" | sudo -S sudo chmod -R 755 ./conf/mariadb
-echo -e "TeddyBear101" | sudo -S sudo touch ./conf/mariadb/.gitkeep
+sudo chown -R 999:999 ./data/mariadb
+sudo chmod -R 755 ./data/mariadb
 
 docker compose up -d
